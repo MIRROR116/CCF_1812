52577238be10da725196c692b51e3a169244a2d5
MIRROR116 <577547640@qq.com>
2019-02-26 17:45:51 +0800

    git commit -m "second"

---
+++ gobackhome/gobackhome/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  gobackhome
+//
+//  Created by mirror_z on 2019/2/22.
+//  Copyright © 2019年 mirror. All rights reserved.
+//
+
+#include <iostream>
+typedef long long  ll;
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
